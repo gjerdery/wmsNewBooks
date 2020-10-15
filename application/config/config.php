b7e9a1a46eea7437cdfcbfb8c7e9db0f19e549e1
gjerdery <gjerdery@luther.edu>
2020-10-15 21:30:29 +0000

    update base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://lis.luther.edu/wmsNewBooks';
+$config['base_url'] = 'https://lis.luther.edu/wmsNewBooks';
 
 /*
 |--------------------------------------------------------------------------
